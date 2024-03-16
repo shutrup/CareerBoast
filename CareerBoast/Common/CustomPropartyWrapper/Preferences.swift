2b1789eced010bf05273afdbbdc2aa3a1351eb44
shutrup <magagaraev95@mail.ru>
2024-03-16 21:33:26 +0300

    configure project

---
+++ CareerBoast/Common/CustomPropartyWrapper/Preferences.swift
@@ -0,0 +1,101 @@
+import SwiftUI
+import Combine
+
+final class Preferences {
+    
+    static let standard = Preferences(userDefaults: .standard)
+    fileprivate let userDefaults: UserDefaults
+    
+    var preferencesChangedSubject = PassthroughSubject<AnyKeyPath, Never>()
+    
+    init(userDefaults: UserDefaults) {
+        self.userDefaults = userDefaults
+    }
+    
+    @UserDefault("userPhone")
+    var userPhone = String()
+    
+    @UserDefault("userCard")
+    var userCard = String()
+    
+    @UserDefault("authStatus")
+    var authStatus = Bool()
+}
+
+@propertyWrapper
+struct UserDefault<Value> {
+    let key: String
+    let defaultValue: Value
+    
+    var wrappedValue: Value {
+        get { fatalError("Wrapped value should not be used.") }
+        set { fatalError("Wrapped value should not be used.") }
+    }
+    
+    init(wrappedValue: Value, _ key: String) {
+        self.defaultValue = wrappedValue
+        self.key = key
+    }
+    
+    public static subscript(
+        _enclosingInstance instance: Preferences,
+        wrapped wrappedKeyPath: ReferenceWritableKeyPath<Preferences, Value>,
+        storage storageKeyPath: ReferenceWritableKeyPath<Preferences, Self>
+    ) -> Value {
+        get {
+            let container = instance.userDefaults
+            let key = instance[keyPath: storageKeyPath].key
+            let defaultValue = instance[keyPath: storageKeyPath].defaultValue
+            return container.object(forKey: key) as? Value ?? defaultValue
+        }
+        set {
+            let container = instance.userDefaults
+            let key = instance[keyPath: storageKeyPath].key
+            container.set(newValue, forKey: key)
+            instance.preferencesChangedSubject.send(wrappedKeyPath)
+        }
+    }
+}
+
+@propertyWrapper
+struct Preference<Value>: DynamicProperty {
+    
+    @ObservedObject private var preferencesObserver: PublisherObservableObject
+    private let keyPath: ReferenceWritableKeyPath<Preferences, Value>
+    private let preferences: Preferences
+    
+    init(_ keyPath: ReferenceWritableKeyPath<Preferences, Value>, preferences: Preferences = .standard) {
+        self.keyPath = keyPath
+        self.preferences = preferences
+        let publisher = preferences
+            .preferencesChangedSubject
+            .filter { changedKeyPath in
+                changedKeyPath == keyPath
+            }.map { _ in () }
+            .eraseToAnyPublisher()
+        self.preferencesObserver = .init(publisher: publisher)
+    }
+
+    var wrappedValue: Value {
+        get { preferences[keyPath: keyPath] }
+        nonmutating set { preferences[keyPath: keyPath] = newValue }
+    }
+
+    var projectedValue: Binding<Value> {
+        Binding(
+            get: { wrappedValue },
+            set: { wrappedValue = $0 }
+        )
+    }
+}
+
+final class PublisherObservableObject: ObservableObject {
+    
+    var subscriber: AnyCancellable?
+    
+    init(publisher: AnyPublisher<Void, Never>) {
+        subscriber = publisher.sink(receiveValue: { [weak self] _ in
+            self?.objectWillChange.send()
+        })
+    }
+}
---
+++ CareerBoast/Models/TestModel.swift
@@ -0,0 +1,8 @@
+//
+//  TestModel.swift
+//  CareerBoast
+//
+//  Created by Шарап Бамматов on 16.03.2024.
+//
+
+import Foundation
