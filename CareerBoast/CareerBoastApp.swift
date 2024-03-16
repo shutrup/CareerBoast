b825dacc820f2fc9e5cb688df42d631c90017c37
shutrup <magagaraev95@mail.ru>
2024-03-16 21:16:57 +0300

    Initial Commit

---
+++ CareerBoast/CareerBoastApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareerBoastApp.swift
+//  CareerBoast
+//
+//  Created by Шарап Бамматов on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CareerBoastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
