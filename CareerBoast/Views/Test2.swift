d09f36d3ee71c75f9cf9e7433412b7eb4338a917
Гамид Халидов <khalidov-gamid@mail.ru>
2024-03-17 14:33:28 +0300

    test2

---
+++ CareerBoast/Views/Test2.swift
@@ -0,0 +1,18 @@
+//
+//  Test2.swift
+//  CareerBoast
+//
+//  Created by Gamıd Khalıdov on 17.03.2024.
+//
+
+import SwiftUI
+
+struct Test2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test2()
+}
