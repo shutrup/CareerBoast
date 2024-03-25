827bc97905f844e10075e01db0143bfcb46316fe
Гамид Халидов <khalidov-gamid@mail.ru>
2024-03-25 17:09:38 +0300

    Убрал кнопку входа в аккаунт. Регистрация работает вместе с обработкой разных ошибок (not valid Email/not valid Password/TextFields is empty)

--- CareerBoast/Views/AuthorizationView.swift
+++ CareerBoast/Views/AuthorizationView.swift
@@ -6,13 +6,20 @@
 //
 
 import SwiftUI
+import Firebase
+
+struct AlertShow: Identifiable {
+    var id: String { name }
+    let name: String
+}
 
 struct AuthorizationView: View {
-    @State private var username = ""
+    @State private var email = ""
     @State private var password = ""
     @State private var wrongUsername = 0
     @State private var wrongPassword = 0
     @State private var showLoginScreen = false
+    @State private var showAlert: AlertShow?
     
     
     var body: some View {
@@ -33,7 +40,9 @@ struct AuthorizationView: View {
                     .bold()
                     .padding()
                 
-                TextField("Username", text: $username)
+                Spacer().frame(height: 10)
+                
+                TextField("Username", text: $email)
                     .padding()
                     .frame(width: 300, height: 50)
                     .background(Color.black.opacity(0.06))
@@ -49,33 +58,56 @@ struct AuthorizationView: View {
                     .background(Color.black.opacity(0.06))
                     .border(.red, width: CGFloat(wrongPassword))
                     .cornerRadius(10, corners: .allCorners)
-
+                
+                Spacer().frame(height: 40)
+                
                 HStack{
-                    Button("Log in"){
+                    
+                    Button("Sign up"){
+                        if (email != "" || password != "") {
+                            wrongPassword = 0
+                            wrongUsername = 0
+                            register()
+                            
+                            
+                        }else{
+                            wrongPassword = 1
+                            wrongUsername = 1
+                            showAlert = AlertShow(name: "Заполните все поля!")
+                        }
                         
                     }
                     .foregroundStyle(Color.white)
-                    .frame(width: 145, height: 50)
+                    .frame(width: 300, height: 50)
                     .background(.blue)
                     .cornerRadius(10, corners: .allCorners)
-                    
-                    Spacer()
-                    
-                    Button("Sign in"){
-                        
+                    .alert(item: $showAlert) { alert in
+                        Alert(title: Text(alert.name), dismissButton: .cancel())
                     }
-                    .foregroundStyle(Color.white)
-                    .frame(width: 145, height: 50)
-                    .background(.blue)
-                    .cornerRadius(10, corners: .allCorners)
-                    
 
 
                 }
                 .frame(width: 300)
             }
         }
-
+    }
+    func register(){
+        Auth.auth().createUser(withEmail: email, password: password){ result, error in
+            if error != nil {
+                print(error?.localizedDescription ?? "")
+                
+                if error?.localizedDescription == "The email address is already in use by another account."{
+                    showAlert = AlertShow(name: "Пользователь с таким электронным адресом уже зарегестрирован.")
+                } else if error?.localizedDescription == "The password must be 6 characters long or more."{
+                    showAlert = AlertShow(name: "Пароль должен быть длиннее 6 символов")
+                } else if error?.localizedDescription == "The email address is badly formatted."{
+                    showAlert = AlertShow(name: "Некорректный Email адрес")
+                }
+            }else{
+                showAlert = AlertShow(name: "Ваш аккаунт успешно зарегестрирован!")
+            }
+            
+        }
         
     }
 }
